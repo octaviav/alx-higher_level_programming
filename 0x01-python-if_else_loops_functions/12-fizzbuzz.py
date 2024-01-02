90e3259a96e393fb0d1c7b7c8969bf95d5b36841
octaviav <octavia7104@gmail.com>
2024-01-02 09:17:03 -0800

    1-100 3x5 Fizz x Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for k in range(1, 101):
+        if k % 3 == 0 and k % 5 != 0:
+            print("Fizz ", end='')
+        elif k % 5 == 0 and k % 3 != 0:
+            print("Buzz ", end='')
+        elif k % 5 == 0 and k % 3 == 0:
+            print("FizzBuzz ", end ='')
+        else:
+            print("{} ".format(k), end='')
