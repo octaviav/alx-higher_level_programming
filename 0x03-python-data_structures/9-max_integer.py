62625c47078fb51fe78569e31a98ae6969b8d535
octaviav <octavia7104@gmail.com>
2024-01-08 10:40:12 -0800

    maximum int in a tuple

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) <= 0:
+        return None
+    else:
+        maximun = my_list[0]
+        for k in range(len(my_list)):
+            if my_list[k] > maximum:
+                maximum = my_list[k]
+        return maximum
