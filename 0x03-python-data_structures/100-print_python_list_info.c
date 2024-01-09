52c5aad9aa9f72e78f2803c0e036755df577b161
octaviav <octavia7104@gmail.com>
2024-01-09 12:26:24 -0800

    info about python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,7 +8,7 @@ void print_python_list_info(PyObject *p)
 	int k;
 	PyListObject *object = (PyListObject *)p;
 
-	printf("[*] Size of the Python Ist = %li\n", siz);
+	printf("[*] Size of the Python List = %li\n", siz);
 	printf("[*] Allocated = %li\n", object->allocated);
 	for (k = 0; k < siz; k++)
 		printf("Element %i: %s\n", k, py_TYPE(object->ob_item[k])->tp_name);
