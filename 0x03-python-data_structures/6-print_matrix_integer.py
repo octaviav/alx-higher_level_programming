e702f28bbf440a2c07d6ae5f34149dd8409909e1
octaviav <octavia7104@gmail.com>
2024-01-09 11:38:48 -0800

    matrix in python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for k in matrix:
             for j in k:
                 print("{:d}".format(j), end=' ' if j != k[-1] else '')
-                print()
+            print()
