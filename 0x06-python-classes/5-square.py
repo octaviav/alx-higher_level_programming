feeda0256c66b3e8164c8dbb1c5a25373f449011
octaviav <octavia7104@gmail.com>
2024-01-24 08:38:40 -0800

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,7 +10,7 @@ class Square:
         """Initialize a new square.
 
         Args:
-            size (int): Size of the new square.
+            size (int): The size of the new square.
         """
         self.size = size
 
@@ -28,13 +28,13 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return current area of the square."""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with # character."""
+        """Print the square with the # character."""
         for k in range(0, self.__size):
-            [print("#", end="") for l in range(self.__size)]
+            [print("#", end="") for j in range(self.__size)]
             print("")
         if self.__size == 0:
             print("")
