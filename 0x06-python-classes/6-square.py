369d62d1c6b7378e6acbc4902b7f010838cde2e2
octaviav <octavia7104@gmail.com>
2024-01-27 06:40:57 -0800

    defining a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,8 +53,8 @@ class Square:
             print("")
             return
 
-        [print("") for k in range(0, self.__position[1])]
-        for k in range(0, self.__size):
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for l in range(0, self.__size)]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
