e2edc4ef83e7451e4926d6448e4d37f43d5162ae
octaviav <octavia7104@gmail.com>
2024-02-07 15:38:14 -0800

    Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,6 @@
 """Defines a class and inherited class-checking function."""
 
 
-
 def is_kind_of_class(obj, a_class):
     """Checks whether an object is an instance or inherited instance of a class.
 
